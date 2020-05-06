3b79d693315c0d8690084b08f98183c5f7df1c34
Viktor <vi.Ivanov28@gmail.com>
2020-05-06 10:36:49 +0300

    Initial Commit

---
+++ TestReaddle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestReaddle
+//
+//  Created by Ivanov Viktor on 06.05.2020.
+//  Copyright © 2020 Ivanov Viktor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
